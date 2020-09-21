769f6239f4d7015b12995dad1e720bd86536a4be
omonaza <aza.osmonbekov93@gmail.com>
2020-09-21 20:38:39 +0400

    update userEmail

--- src/JiraProject/userEmail.java
+++ src/JiraProject/userEmail.java
@@ -8,19 +8,40 @@ public class userEmail {
 
     public static void main(String[] args) {
 
+//        Scanner scn = new Scanner(System.in);
+//        System.out.println("Enter your email: ");
+//        String email = "";
+//        while (email != Exception) {
+//            email = scn.next();
+//            try {
+//             String [] str =  email.split(String.valueOf(email.indexOf("@")));
+//             for (int i = 0; i < email.length();i++){
+//
+//             }
+//
+//                break;
+//            } catch (Exception e) {
+//                System.out.println("Please Enter again valid email address,\nbecause of you people entering invalid address, we have to struggle to fix this )))");
+//            }
+//        }
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter your email: ");
         String email = "";
-        while (email != Exception) {
+        while (email!=Exception) {
+          //  System.out.println("Enter your email: ");
             email = scn.next();
             try {
-                System.out.println("Email id: " + email.substring(0, email.indexOf("@")) +
-                        "\ndomain: " + email.substring(email.indexOf(("@"))));
-                break;
+                if (email.indexOf('@') == 0) {
+                    System.out.println(" Please Enter valid email address: ");
+                  //  email = scn.next();
+                } else {
+                    System.out.println("Email id: " + email.substring(0, email.indexOf("@")) +
+                  "\ndomain: " + email.substring(email.indexOf(("@"))));
+                    break;
+                }
             } catch (Exception e) {
-                System.out.println("Please Enter again valid email address,\nbecause of you people entering invalid address, we have to struggle to fix this )))");
+                System.out.println("Please Enter valid email address,\nbecause of you people entering invalid address, we have to struggle to fix this )))");
             }
         }
-
     }
 }
